96a725edb6a33f6a15ae7f6574eeb34caa0b6989
madcreep <madcreep@biz-it.ru>
2018-09-24 22:55:03 +0300

    Мелкие исправления

--- src/eos-dictionaries/consts/dictionaries/_linear-template.ts
+++ src/eos-dictionaries/consts/dictionaries/_linear-template.ts
@@ -11,7 +11,7 @@ export const LINEAR_TEMPLATE: IDictionaryDescriptor = {
     actions: [
         'add', 'markRecords', 'quickSearch', 'fullSearch', 'order', 'userOrder', 'restore',
         'moveUp', 'moveDown', 'navigateUp', 'navigateDown', 'showDeleted', 'removeHard',
-        'edit', 'view', 'remove', 'removeHard', 'userOrder'],
+        'edit', 'view', 'remove', 'removeHard', 'userOrder', 'tableCustomization'],
     keyField: 'ISN_LCLASSIF',
     searchConfig: [SEARCH_TYPES.quick],
     fields: COMMON_FIELDS.concat([{
