bc70b6f9c6252d71d100ccb84c3c9acbaa9bd6fe
hedgehog <derevenskihav@biz-it.ru>
2018-11-15 11:48:25 +0300

    Bug 93815:Виды подписей. Наименование -> Вид подписи

--- src/eos-dictionaries/consts/dictionaries/sign-kind.consts.ts
+++ src/eos-dictionaries/consts/dictionaries/sign-kind.consts.ts
@@ -1,7 +1,8 @@
-import { IDictionaryDescriptor, E_DICT_TYPE } from 'eos-dictionaries/interfaces';
-import { NOT_EMPTY_STRING } from '../input-validation';
-import { SEARCH_TYPES } from '../search-types';
-import { LINEAR_TEMPLATE } from './_linear-template';
+import {IDictionaryDescriptor, E_DICT_TYPE} from 'eos-dictionaries/interfaces';
+import {NOT_EMPTY_STRING} from '../input-validation';
+import {SEARCH_TYPES} from '../search-types';
+import {LINEAR_TEMPLATE} from './_linear-template';
+import {COMMON_FIELD_NAME} from './_common';
 /*
 */
 export const SIGN_KIND_DICT: IDictionaryDescriptor = {
@@ -13,19 +14,24 @@ export const SIGN_KIND_DICT: IDictionaryDescriptor = {
     visible: true,
     iconName: 'eos-icon-signature-blue',
     actions: [
-        'markRecords', 'quickSearch', 'fullSearch', 'order', 'userOrder',
-        'moveUp', 'moveDown', 'navigateUp', 'navigateDown', 'showDeleted', 'tableCustomization', /*'edit',*/ 'view',
+        'quickSearch', 'fullSearch', 'order', 'userOrder',
+        'moveUp', 'moveDown', 'navigateUp', 'navigateDown', 'tableCustomization', /*'edit',*/ 'view',
         'restore'
     ],
     keyField: 'ISN_LCLASSIF',
     searchConfig: [SEARCH_TYPES.quick],
-    fields: LINEAR_TEMPLATE.fields.concat([{
-        key: 'SIGN_TEXT',
-        title: 'Текст подписи',
-        type: 'text',
-        length: 255,
-        pattern: NOT_EMPTY_STRING,
-    }]),
+    fields: LINEAR_TEMPLATE.fields.concat([
+        Object.assign({}, COMMON_FIELD_NAME, {
+            title: 'Вид подписи',
+        }),
+        {
+            key: 'SIGN_TEXT',
+            title: 'Текст подписи',
+            type: 'text',
+            length: 255,
+            pattern: NOT_EMPTY_STRING,
+        }
+    ]),
     treeFields: ['CLASSIF_NAME'],
     editFields: ['CLASSIF_NAME', 'SIGN_TEXT'],
     searchFields: ['CLASSIF_NAME', 'SIGN_TEXT'],
